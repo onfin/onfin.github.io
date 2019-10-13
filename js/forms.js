e442de02d6ff038812ba7a0fa0d24be8d1e349c8
Celsior <celsior.izh@gmail.com>
2019-10-13 18:58:42 +0400

    Restyling and calculators.

--- js/forms.js
+++ js/forms.js
@@ -1,3 +1,38 @@
+const {h, render, Fragment} = preact;
+const {useState} = preactHooks;
+
+function makeForm(div, fields, calc) {
+    const defaultValues = {};
+    for (let k in fields) {
+        defaultValues[k] = fields[k].default;
+        if (!fields[k].type)
+            fields[k].type = "number";
+    }
+    calc(defaultValues);
+    const PForm = () => {
+        const [values, setValues] = useState(defaultValues);
+        const onChange = (name, value) => {
+            const newValues = {...values, [name]: value};
+            calc(newValues);
+            setValues(newValues);
+        }
+        const res = [];
+        for (let k in fields) {
+            if (!fields[k].output)
+                res.push(h('tr', null,
+                    h('td', null, fields[k].title),
+                    h('td', null, h('input', {...fields[k], [fields[k].type == "checkbox" ? 'checked' : 'value']: values[k], onInput: (e) => onChange(k,e.target[fields[k].type == "checkbox" ? 'checked' : 'value'])}))));
+            else
+                res.push(h('tr', null,
+                    h('td', null, fields[k].title),
+                    h('td', null, h('input', {...fields[k], value: values[k], readonly: true, class: "output"}))));
+        }
+        return h(Fragment, {}, res);
+    }
+
+    render(h(PForm), div);
+}
+
 function Form(name, fields, calc) {
     this.form = document.getElementById(name);
     this.fields = fields;
